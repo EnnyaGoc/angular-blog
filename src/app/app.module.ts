f43acc6bc644e21145cb888bcb0fd89f3af94db0
Ênnya Gomes <ennyagomesoc@gmail.com>
2024-02-25 03:08:25 -0300

    content created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { MenuTitleComponent } from './components/menu-title/menu-title.component
 import { SmallCardComponent } from './components/small-card/small-card.component';
 import { BigCardComponent } from './components/big-card/big-card.component';
 import { HomeComponent } from './pages/home/home.component';
+import { ContentComponent } from './pages/content/content.component';
 
 @NgModule({
   declarations: [
@@ -17,7 +18,8 @@ import { HomeComponent } from './pages/home/home.component';
     MenuTitleComponent,
     SmallCardComponent,
     BigCardComponent,
-    HomeComponent
+    HomeComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,14 @@
+export const dataFake = [
+    {
+        "id":"1",
+        "title":"novo homem de ferro",
+        "description":"marvel anuncia blablabla",
+        "photoCover":""
+    },
+    {
+        "id":"2",
+        "title":"blablabla",
+        "description":"blablablablabla",
+        "photoCover":""
+    }
+]
